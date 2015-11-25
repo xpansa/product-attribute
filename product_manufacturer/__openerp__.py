4737be0d998a940e6ddee6f035b15f57756de251
Rohan <tidyway.in@gmail.com>
2015-11-25 10:49:11 +0530

    [MIGRATED]product_manufacturer module migrated to v9

--- product_manufacturer/__openerp__.py
+++ product_manufacturer/__openerp__.py
@@ -19,9 +19,9 @@
 ##############################################################################
 {
     'name': 'Products Manufacturers',
-    'version': '8.0.1.0.0',
+    "version": "9.0.1.0.0",
+    "license": "LGPL-3",
     'author': "OpenERP SA,Odoo Community Association (OCA)",
-    'license': 'AGPL-3',
     'contributors': ['Acysos SL <info@acysos.com>'],
     'category': 'Purchase Management',
     'depends': ['product'],
